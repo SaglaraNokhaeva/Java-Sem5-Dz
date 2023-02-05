22c0153a20cac1d01e50896b4541b10b38882982
Saglara <nohaevass@yandex.ru>
2023-02-05 15:13:25 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -13,54 +13,60 @@ public class task1 {
 
         LinkedList<String> intList = new LinkedList<String>();
         Map<Integer, LinkedList<String>> listMap = new HashMap<Integer, LinkedList<String>>();
+        int key=0;
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите фамилию контакта: ");
-        String sekond_name = in.nextLine();
-        System.out.print("Введите Имя контакта: ");
-        String first_name = in.nextLine();
-        
-        
         while (true) {
-
-            System.out.println("Введите команду:'ok' для добавления контакта;'+' ввода ещё одного номера: ");
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите команду:'add' - для добавление нового контакта; 'stop' - для выхода: ");
             String str = in.nextLine();
-            if (str.equals("ok"))
 
+            if (str.equals("add")) {
+                key++;
+                System.out.print("Введите фамилию контакта: ");
+                String sekond_name = in.nextLine();
+                System.out.print("Введите Имя контакта: ");
+                String first_name = in.nextLine();
+                System.out.print("Введите номер телефона: ");
+                String phone = in.nextLine();
+                intList.add(sekond_name);
+                intList.add(first_name);
+                intList.add(phone);
+
+                // добавление нескольких номеров, если они имеются
+                while (true) {
+                    System.out
+                            .println(
+                                    "Введите команду:'+' - для ввода ещё одного номера; 'ok' - для добавления контакта: ");
+                    str = in.nextLine();
+                    if (str.equals("+")) {
+                        System.out.print("Введите номер телефона: ");
+                        phone = in.nextLine();
+                        intList.add(phone);
+                    }
+                    if (str.equals("ok")) {
+                        break;
+                    }
+                }
+
+                listMap.putIfAbsent(key, intList);
+                System.out.println(listMap);
+            String abon = listMap.toString();  
+            try (FileWriter writer = new FileWriter("phoneBook.txt", true)) {
+                writer.append(abon);                
+                writer.append('\n');
+
+                writer.flush();
+            } catch (Exception e) {
+
+                System.out.println("что-то не так");
+            }
+            if (str.equals("stop")) {
                 break;
-            
-            if (str.equals("+")) {
-                stack.push(Calcul());
+            }   
+                            
             }
             
-            if (str.equals("cancel")) {
-                stack.pop();
-                System.out.println(stack.peek());
-            }             
-        }
-                  
-        
-        System.out.print("Введите номер телефона: ");
-        String phone = in.nextLine();
-
-        intList.add(sekond_name);
-        intList.add(first_name);
-        intList.add(phone);
-        listMap.put(1, intList);
-        System.out.println(listMap);
-        String abon = listMap.toString();
-
-        try (FileWriter writer = new FileWriter("phoneBook.txt", true)) {
-            writer.append(abon);
-            // writer.write(text);
-            writer.append('\n');
-
-            writer.flush();
-        } catch (Exception e) {
-
-            System.out.println("что-то не так");
 
         }
-
     }
 }

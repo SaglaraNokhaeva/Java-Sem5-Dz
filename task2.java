1a0925e7355eed9b05c6d698387ae97f52d667d5
Saglara <nohaevass@yandex.ru>
2023-02-05 21:23:37 +0300

    Update task2.java

--- task2.java
+++ task2.java
@@ -25,47 +25,43 @@ import java.util.List;
 import java.util.ArrayList;
 import java.sql.Array;
 import java.util.Arrays;
-
+import java.util.Comparator;
+import java.util.Comparator;
 
 public class task2 {
     public static void main(String[] args) {
-        String spisok = ("Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов");
-        // ArrayList<String> list = new ArrayList<String>();
+        String spisok = ("Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов, Светлана Рыжова");
+        // Сплитуем по запятой
         String[] array = spisok.split("\\, | ");
         int razmernost = array.length;
-        String[] array_name = new String[razmernost];
-        int j=0;
-        for (int i = 0; i < razmernost; i += 2) {
-                if (array_name[j]!=null){            
-                array_name[j] = array[i];
-                }j++;
+        System.out.println(razmernost);
+        // Создаём лист с именами
+        String[] array_name = new String[razmernost / 2];
+        int j = 0;
+        for (int i = 0; i < razmernost / 2; i++) {
+            array_name[i] = array[j];
+            j += 2;
             System.out.println(array_name[i]);
         }
         ArrayList<String> strList = new ArrayList<String>();
         for (int i = 0; i < array_name.length; i++) {
             strList.add(array_name[i]);
-        }        
+        }
+        // Сортировка по алфавиту
+        strList.sort(Comparator.naturalOrder());
         System.out.print("The ArrayList is: " + strList);
-
-
-
-        
-        // list.add(spisok);
-        // System.out.println(list);
-
-
-
-
-        // String spisok = ("Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов");
-        // // формируем список имён
-        // String[] array = spisok.split("\\, | ");
-        // int razmernost = array.length;
-        // String[] array_name = new String[razmernost];
-        // for (int i = 0; i < razmernost; i += 2) {
-        //     for (int j = 0; j < razmernost / 2; j++)
-        //         array_name[i] = array[i];
-        //     System.out.println(array_name[i]);
-        // }
-
+        System.out.println();
+        int count=1;
+        for (int i = 0; i < razmernost / 2-1; i++) {
+            
+            if (strList.get(i).equals(strList.get(i+1))){
+                count++;                
+            }
+            else{
+                System.out.printf("%s = %d; \n",strList.get(i),count);
+                count=1;
+            }           
+        }
+        System.out.printf("%s = %d; \n",strList.get(razmernost / 2-1),count);
     }
 }
